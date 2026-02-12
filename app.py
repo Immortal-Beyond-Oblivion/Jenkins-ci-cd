63c751d81eb96fceb2bd5c52b729bcd5a79e5888
Immortal-Beyond-Oblivion <yourlimitisyourknowledge@gmail.com>
2026-02-12 23:24:46 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     build_id = os.getenv('BUILD_ID', 'Local')
-    return f"<h1>Hello from Flask!</h1><p>Deployed via Jenkins Webhooks! Build ID : {build_id}.\nThis new change should happen automatically. </p>"
+    return f"<h1>Hello from Flask!</h1><p>Deployed via Jenkins Webhooks! Build ID : {build_id}.\nThis new change should happen automatically.\nThis should be automatically be built by jenkins via webhooks. </p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
